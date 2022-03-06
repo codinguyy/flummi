3a7f6397bea909d768efc90b91d04e563e720812
tederean <tederean@gmail.com>
2022-03-06 20:27:57 +0100

    Extend gauge test

--- test/gauge/index.js
+++ test/gauge/index.js
@@ -4,12 +4,25 @@ var testGauge = Gauge(
   document.getElementById("testGauge"),
   {
     max: 100,
-    value: 50
+    value: 50,
+    color: function (value) {
+      if (value < 60) {
+        return "#5ee432"; // green
+      } else if (value < 70) {
+        return "#fffa50"; // yellow
+      } else if (value < 90) {
+        return "#f7aa38"; // orange
+      } else {
+        return "#ef4655"; // red
+      }
+    },
+    label: function (value) {
+      return Math.round(value) + " °C";
+    }
   }
 );
 
-(function loop()
-{
+(function loop() {
   var testValue = Math.random() * 100;
 
   testGauge.setValueAnimated(testValue, 1.5);
